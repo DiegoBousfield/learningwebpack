d7f905258382a4e6302b7d0d3ce2e6db474a1e9d
Diego Bousfield <diegobousfield@gmail.com>
2018-02-02 17:48:43 -0200

    Add button to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import sum from './sum';
-import './image_viewer.js';
-
-const total = sum(10, 10);
-console.log(total);
+const button = document.createElement('button');
+button.innerText = "Click Me";
+button.onclick () => {};
+document.appendElement(button);
